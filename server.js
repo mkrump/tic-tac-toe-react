d161fe5fa7716310433863ac4c622d41699f64c2
mkrump <mkrump@8thlight.com>
2017-06-20 16:45:19 -0500

    Host on Heroku MK-94
    - https://chicagotechgrowhouse.atlassian.net/browse/MKA-94
    - Updated packge.json info
    - Hosted on Heroku @ https://gentle-ocean-96304.herokuapp.com/

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(public_path, 'index.html'));
 });
 
-app.listen(app.get('port'), function() {
+app.listen(process.env.PORT || app.get('port'), function() {
   console.log('React sample dev server started on port ' + app.get('port'));
 });
